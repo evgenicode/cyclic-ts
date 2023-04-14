8ad903c2828cd56208556cf248a99d306586727f
evmatv <evgenii.contact@gmail.com>
2023-04-14 16:30:16 +0300

    deployment

--- src/data/getLocalData.ts
+++ src/data/getLocalData.ts
@@ -2,18 +2,16 @@ import fs from "fs";
 import { hasHealthTypeCode } from "../interfaces/interfaces";
 import { heartRateFilter } from "../filters/heartRateFilter";
 import { sleepDataFilter } from "../filters/sleepDataFilter";
-const path = require("path");
 
 const LOCAL_DATA_STORAGE = process.env.LOCAL_DATA_STORAGE;
-const filePath = path.join(__dirname, LOCAL_DATA_STORAGE);
-//test
+
 if (!LOCAL_DATA_STORAGE) {
   throw new Error(
     "LOCAL_DATA_STORAGE is not defined in environment variables."
   );
 }
 
-export const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+export const data = JSON.parse(fs.readFileSync(LOCAL_DATA_STORAGE, "utf-8"));
 
 export const heartRateData = data.filter(
   (item: hasHealthTypeCode) => item.type === 7
